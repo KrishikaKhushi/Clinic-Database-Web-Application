64c0de4b045844193aba908a463505e595076add
Krishika Khushi <krishikakhushi12@gmail.com>
2025-08-16 20:23:53 +0530

    Fix CORS for production deployment

--- server/server.js
+++ server/server.js
@@ -9,7 +9,14 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+    'https://clinic-management-system-inky-rho.vercel.app',
+    'http://localhost:5173',
+    'http://localhost:3000'
+  ],
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
